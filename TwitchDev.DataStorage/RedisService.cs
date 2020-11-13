85a4320f183b86f493eea67533ec96d43c091f56
Billou13 <billou13@gmail.com>
2020-11-13 11:36:00 +0100

    Add installation step guide for redis on docker

--- TwitchDev.DataStorage/RedisService.cs
+++ TwitchDev.DataStorage/RedisService.cs
@@ -7,8 +7,21 @@ using TwitchDev.DataStorage.Interfaces;
 namespace TwitchDev.DataStorage
 {
     /// <summary>
-    /// To install redis, watch the following video: <see cref="https://youtu.be/YhXeiB_1-uk"/>.
+    /// Redis on Docker installation guide (done on windows 10):
+    ///   - Install Docker Desktop from <see cref="https://www.docker.com/get-started"/>
+    ///   - Open a command prompt and run the following commands:
+    ///       # Download redis image from docker hub
+    ///       docker pull redis
+    ///       
+    ///       # Run redis image exposing port 6379 to 6379 on host machine
+    ///       docker run -d -p 6379:6379 --name my-redis redis
+    ///       
+    ///       # Show docker container to see that status is up
+    ///       docker ps -l
+    /// 
+    /// Source: thank you to <see cref="https://youtu.be/YhXeiB_1-uk"/>.
     /// </summary>
+    /// <seealso cref="https://docs.docker.com/engine/reference/commandline/docker"/>
     public class RedisService : IRedisService
     {
         private readonly RedisConfiguration _configuration;
